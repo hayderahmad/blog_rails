887a6b4696f9db187cadb91aec7e1c5b81a0ca77
hayderahmad@yahoo.com <hayderahmad@yahoo.com>
2022-06-03 18:10:11 -0600

    3rd commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+Rails.application.config.hosts << "0c87-205-204-35-42.ngrok.io"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
